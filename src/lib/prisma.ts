181f405c3e110d0c30a76470d0bffa024e94406e
AlejandroCosta02 <costa.claudio.alejandro@gmail.com>
2025-05-29 14:45:43 -0300

    Simplified Prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient({
-  log: ['error', 'warn'],
-})
-
-if (process.env.NODE_ENV !== 'production') {
-  prisma.$on('query', (e: any) => {
-    console.log('Query:', e.query)
-    console.log('Duration:', e.duration + 'ms')
-  })
-}
+const prisma = new PrismaClient()
 
 export const db = prisma
 export default prisma 

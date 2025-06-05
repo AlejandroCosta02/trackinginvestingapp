98f941e1c6e0a7fe53f2fd7e732537e19265758e
AlejandroCosta02 <costa.claudio.alejandro@gmail.com>
2025-06-05 15:09:42 -0300

    fix: Update Prisma client initialization

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+declare global {
+  var prisma: PrismaClient | undefined
+}
 
-export const db = prisma
-export default prisma 
+const prisma = global.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+
+export const db = prisma 
